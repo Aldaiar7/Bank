66f358ef08c30c95a9721b3b4011c826342ccfa7
Aldaiar <aldaiardushebaev@gmail.com>
2022-04-15 17:16:08 +0600

    delete cart

--- CartApp/urls.py
+++ CartApp/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('carts/', CartListView.as_view()),
     path('cart/<int:pk>/', CartView.as_view()),
-    path('order/', OrderListCreateView.as_view())
+    path('order/', OrderListCreateView.as_view()),
 ]
--- CartApp/views.py
+++ CartApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework import filters
+from rest_framework.response import Response
+
 from .models import Cart
 from .serializers import *
 
@@ -12,14 +14,22 @@ class CartListView(generics.ListAPIView):
     serializer_class = CartSerializer
 
 
-class CartView(generics.ListAPIView):
+class CartView(generics.ListAPIView, generics.DestroyAPIView):
     serializer_class = CartProfileSerializer
 
     def get_queryset(self):
         queryset = Cart.objects.filter(pk=self.kwargs['pk'])
         return queryset
 
+    def destroy(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object())
+        super().destroy(args, kwargs)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+
